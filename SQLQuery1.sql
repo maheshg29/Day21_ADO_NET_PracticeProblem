a9c3e53a0b23f0a6da583a9581f178c6b9b0e22c
mahesh <gangurdemahesh9@gmail.com>
2023-04-18 21:06:13 +0530

    [mahesh]add program file for Update Customer Salary

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,32 @@
 create database Day21_ADOdotNET
 use Day21_ADOdotNET
+
+create table Customer(
+Id int identity(1,1) primary key,
+Name varchar(200),
+City varchar(200),
+Address varchar(200),
+MobileNumber bigint)
+
+alter table Customer add Salary bigint;
+select * from Customer;
+
+CREATE PROCEDURE spAddCustomer
+@Name varchar(30),
+@City varchar(30),
+@Address varchar(100),
+@Phone bigint,
+@Salary bigint
+AS
+INSERT INTO Customer(Name,City,Address,MobileNumber,Salary) VALUES (@Name,@City,@Address,@Phone,@Salary)
+
+CREATE PROCEDURE spDeleteCustomer
+@Name varchar(30)
+AS
+delete from Customer where Name=@Name
+
+CREATE PROCEDURE spUpdateSalary
+@Name varchar(30),
+@Salary bigint
+AS
+UPDATE Customer SET Salary=@Salary where Name=@Name
