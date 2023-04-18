f9ae187bdb07c3ca59f00afdaf437826966e87f7
mahesh <gangurdemahesh9@gmail.com>
2023-04-18 19:56:46 +0530

    [mahesh]add program file for add Welcome Greeting Message

---
+++ Day21_ADO_NET_PracticeProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21_ADO_NET_PracticeProblem
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 21 ADO.NET Practice problem");
+        }
+    }
+}
